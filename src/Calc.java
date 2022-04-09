1a48e7eee48121096763a93b94075a839d3b7dcb
KryukIvan <nover1313@gmail.com>
2022-04-09 17:29:06 +0300

    Initial commit

---
+++ src/Calc.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Calc {
+    public static void main (String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int x = in.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int y = in.nextInt();
+
+        System.out.print("Введите знак действия `+` `-` `/` или `*`");
+
+        String z = in.next();
+
+        switch(z) {
+
+            case "+":
+                int k = x +y;
+                System.out.println("Сумма равна: " + k);
+                break;
+
+            case "-":
+                int m = x - y;
+                System.out.println("Разность равна: " + m);
+                break;
+
+            case "/":
+                int l = x / y;
+                System.out.println("Частное равно: " + l);
+                break;
+
+            case "*":
+                int f = x * y;
+                System.out.println("Произведение равно: " + f);
+                break;
+        }
+
+
+    }
+
+}
